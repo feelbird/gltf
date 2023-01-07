0d425efc596a1851e47ca6f39b04629f41f1b714
feelbird <41306505+feelbird@users.noreply.github.com>
2023-01-07 06:25:47 +0100

    Update and rename pixelart.js to pixels.js

--- pixels.js
+++ pixels.js
@@ -68,10 +68,8 @@ fetch('https://uploads-ssl.webflow.com/637d37d31d152db9a1d8b984/63b8e87646576d2b
         }
       })
 
-.fromTo(pivot.rotation, {y: 0, duration: 2}, {y: 0, duration: 2})
-.fromTo(pivot.rotation, {y: 0, duration: 8}, {y: 2 * Math.PI, ease: "Power2.easeInOut", duration: 8})
-.fromTo("#pixel", {opacity: 0, y: -100, duration: 2}, {opacity: 0, y: -100, duration: 2})
-.fromTo("#pixel", {opacity: 0, y: -100, duration: 8}, {opacity: 1, y: 500, duration: 8});
+.fromTo(pivot.rotation, {y: 0, duration: 0.1}, {y: 2 * Math.PI, ease: "Power2.easeInOut", duration: 8})
+.fromTo("#app", {opacity: 0, y: -200, duration: 0.1}, {opacity: 1, y: 500, duration: 8},"<");
         })
   });
 
