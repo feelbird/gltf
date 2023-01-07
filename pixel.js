ee482fe9b6165d3c7484788f7073909858bf4bfd
feelbird <41306505+feelbird@users.noreply.github.com>
2023-01-07 04:41:13 +0100

    pixel art js

--- pixel.js
+++ pixel.js
@@ -3,34 +3,26 @@ import { OrbitControls } from 'https://cdn.skypack.dev/three@0.129.0/examples/js
 import { GLTFLoader } from 'https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js';
 
 // Create Scene + Camera
-const container = document.getElementById("app2");
+const container = document.getElementById("8bit");
 const mainScene = new THREE.Scene();
 const mainCamera = new THREE.PerspectiveCamera(
-  90,
-  container.offsetWidth / container.offsetHeight,
-  0.1,
-  1000
-);
-mainCamera.position.set(0, 2, 4);
+    65,
+    container.offsetWidth / container.offsetHeight,
+    0.1,
+    20
+  );
+  mainCamera.position.set(0, 0, 0.5);
 
-const ambientlight = new THREE.AmbientLight(0xFFFFFF, 0.5);
+// Add Point Lights
+
+const ambientlight = new THREE.AmbientLight(0x943b41, 0.5);
 mainScene.add(ambientlight);
 
-const fillLight = new THREE.PointLight(0x7a5335, 15, 20);
-fillLight.position.set(0,1,4);
-mainScene.add(fillLight);
+const directionalLight = new THREE.DirectionalLight( 0xffd4d7, 3 );
+directionalLight.position.set(6, 10, 5)
+mainScene.add( directionalLight );
 
-const backLight = new THREE.PointLight(0x7a5335, 6, 30);
-backLight.position.set(2, 2, -2);
-mainScene.add(backLight);
-
-const backLightleft = new THREE.PointLight(0xc77e63, 3, 20);
-backLightleft.position.set(-2, 2, -2);
-mainScene.add(backLightleft);
-
-const rightLight = new THREE.PointLight(0x7a5335, 6, 50);
-rightLight.position.set(3,3,1);
-mainScene.add(rightLight);
+// Create Renderer
 
 const renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true });
 renderer.setSize(container.offsetWidth, container.offsetHeight);
@@ -39,9 +31,11 @@ renderer.physicallyCorrectLights = true;
 renderer.outputEncoding =  THREE.sRGBEncoding;
 renderer.gammaFactor =  2.2;
 container.appendChild(renderer.domElement);
+
 // Load 3D Model
+
 const loader = new GLTFLoader(); 
-fetch('https://uploads-ssl.webflow.com/637d37d31d152db9a1d8b984/63b4b8318991fae567d39b48_burger.glb.txt')
+fetch('https://uploads-ssl.webflow.com/637d37d31d152db9a1d8b984/63b8e87646576d2b8794d0c1_pixelart.glb.txt')
   .then(response => response.arrayBuffer())
   .then((arrayBuffer) => {
 
@@ -70,17 +64,23 @@ fetch('https://uploads-ssl.webflow.com/637d37d31d152db9a1d8b984/63b4b8318991fae5
           scrub: 3
         }
       })
-.fromTo("#app2",      
+.fromTo(pivot.rotation,
+      
+              { y: 0, duration: 5 },
+              { y: 2 * Math.PI, ease: "Power2.easeInOut", duration: 5 },
             
-              { opacity:0, y: -150 },
-              { opacity:1, y: 100, duration: 9 }           
+            "<"
             )
-    .fromTo(pivot.rotation,{ y: 0 },{ y: 2 * Math.PI, ease: "Power2.easeInOut", duration: 10 },
-       "<")
-            .to("#app2",{ scale: 0.7, opacity:0, duration: 5 }           
-          );
+            
+            .fromTo("#app", 
+
+            { scale: 1, y: 100, duration: 5  },
+            { scale: 0.7, y: 500, duration: 5 }, "<"           
+          )
         })
   });
+
+  
 // Render Scene
 function render() {
   renderer.render(mainScene, mainCamera);
