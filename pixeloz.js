fc3c2e0751aa20e2b67e1119569107f64fb94c71
feelbird <41306505+feelbird@users.noreply.github.com>
2023-01-17 14:58:54 +0100

    Update and rename pixelzz.js to pixeloz.js

--- pixeloz.js
+++ pixeloz.js
@@ -48,7 +48,7 @@ fetch("https://uploads-ssl.webflow.com/637d37d31d152db9a1d8b984/63b8e87646576d2b
                                                 trigger: ".about__container",
                                                 start: "top-=20% top+=30% ",
                                                 end: "bottom top+=31%",
-                                                markers: 1,
+                                                markers: !1,
                                                 scrub: 2
                                         }
                                 })
