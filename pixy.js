a54080bd5a42480c292dd1d8f417ce7127381711
feelbird <41306505+feelbird@users.noreply.github.com>
2023-01-18 16:00:18 +0100

    Update pixy.js

--- pixy.js
+++ pixy.js
@@ -46,8 +46,8 @@ fetch("https://uploads-ssl.webflow.com/637d37d31d152db9a1d8b984/63b8e87646576d2b
                                         scrollTrigger:
                                         {
                                                 trigger: ".about__container",
-                                                start: "top-=20% top+=10% ",
-                                                end: "bottom+=10% top+=10%",
+                                                start: "top-=20% top+=20% ",
+                                                end: "bottom+=10% top+=20%",
                                                 markers: !1,
                                                 scrub: 2
                                         }
